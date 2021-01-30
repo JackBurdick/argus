2bfc5bae21afde324514be384d9ba8f446fa61c3
Jack Burdick <jackbburdick@gmail.com>
2021-01-30 13:45:06 -0800

    Include camera restart

--- src/app/cam.py
+++ src/app/cam.py
@@ -1,13 +1,22 @@
 import camera
+import machine
+import time
 
 
 def setup_cam(logger):
     QUALITY = 12
-    camera.init(0)
-    logger.debug("Camera initialized")
-    camera.quality(QUALITY)
-    logger.debug("Camera quality set to {}".format(QUALITY))
-    return camera
+    try:
+        camera.init(0)
+    except OSError as e:
+        # TODO: there should be a safeguard here to prevent constant restart
+        time.sleep(2)
+        logger.error("unable to initialize camera. restarting.\n err: {}".format(e))
+        machine.reset()
+    else:
+        logger.debug("Camera initialized")
+        camera.quality(QUALITY)
+        logger.debug("Camera quality set to {}".format(QUALITY))
+        return camera
 
 
 if __name__ == "__main__":
